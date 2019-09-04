c658dff9bb7a887ed82ebf4557de11e787751c5c
GioRubioHolberton <759@holbertonschool.com>
2019-09-04 15:47:34 +0000

    HBNB

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,20 +1,19 @@
 #!/usr/bin/python3
-# task 0
-"""module web_flask
-"""
+"""starts a Flask web application"""
+
 from flask import Flask
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 
-@app.route('/')
+@app.route("/")
+def hi():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb")
 def hello():
-    return 'Hello HBNB!'
-
-
-@app.route('/hbnb')
-def hello2():
-    return 'HBNB'
+    return "HBNB"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=5000)
