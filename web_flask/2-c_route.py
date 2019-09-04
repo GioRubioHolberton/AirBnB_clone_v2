36d45980bc625a82a205bb2bf0cce7415fb6cfad
GioRubioHolberton <759@holbertonschool.com>
2019-09-04 16:10:14 +0000

    C is fun!

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,27 +1,27 @@
 #!/usr/bin/python3
 # task 0
-"""module web_flask
-"""
+"""module web_flask C is fun!"""
+
 from flask import Flask, escape
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 
-@app.route('/')
+@app.route("/")
+def hi():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb")
 def hello():
-    return 'Hello HBNB!'
+    return "HBNB"
 
 
-@app.route('/hbnb')
-def hello2():
-    return 'HBNB'
-
-
-@app.route('/c/<text>')
-def hello3(text):
+@app.route("/c/<text>")
+def func(text):
     text = text.replace("_", " ")
-    return 'C %s' % escape(text)
+    return "C {}".format(text).replace("_", " ")
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=5000)
