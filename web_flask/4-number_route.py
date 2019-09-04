6afcdcd9e88738e2a8c6e89b560ee2f3db360a60
GioRubioHolberton <759@holbertonschool.com>
2019-09-04 16:43:28 +0000

    Is it a number?

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,45 +1,37 @@
 #!/usr/bin/python3
-# task 0
-"""module web_flask
-"""
+"""module web_flask Is it a number?"""
+
 from flask import Flask, escape
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 
-@app.route('/')
+@app.route("/")
+def hi():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb")
 def hello():
-    return 'Hello HBNB!'
+    return "HBNB"
 
 
-@app.route('/hbnb')
-def hello2():
-    return 'HBNB'
-
-
-@app.route('/c/<text>')
-def hello3(text):
+@app.route("/c/<text>")
+def func(text):
     text = text.replace("_", " ")
-    return 'C %s' % escape(text)
+    return "C {}".format(text).replace("_", " ")
 
 
-@app.route('/python/<text>')
-def hello4(text):
-    text = text.replace("_", " ")
-    return 'Python %s' % escape(text)
+@app.route("/python")
+@app.route("/python/<text>")
+def coolpython(text="is cool"):
+    return "Python {}".format(text).replace("_", " ")
 
 
-@app.route('/python/')
-def hello5():
-    text = "is cool"
-    text = text.replace("_", " ")
-    return 'Python %s' % escape(text)
-
-
-@app.route('/number/<int:n>')
-def hello6(n):
-    return '%d is a number' % n
+@app.route("/number/<int:n>")
+def num(n):
+    return "{} is a number".format(n)
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=5000)
